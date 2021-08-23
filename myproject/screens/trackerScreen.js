b0f8f3e32b29a80ed5708f263d7fe9ae900ffda0
omar <omarbdllh1@gmail.com>
2021-08-23 19:50:55 +0300

    version with the app getting the data values

--- myproject/screens/trackerScreen.js
+++ myproject/screens/trackerScreen.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 import { globalStyle } from '../styles/globalStyle';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const Tracker = () =>{
+
+    const [country, setcountry] =  useState("");
+    const storeData = async () =>{
+        //Store only if submitted a country
+        if(country !== ""){
+            try{
+                //initializing 'USER' as a key
+                await AsyncStorage.setItem('USER', country);
+                console.log('Succesful from storeData', country);
+            }catch(e){
+                console.log("Error from storeData: ", e);
+            }
+        }else{
+            console.log("Input field empty....")
+        }
+    }
+
+    const getData = async () =>{
+        const data = await AsyncStorage.getItem('USER');
+        try{
+            //if retrieving successful
+            if(data !== null){
+                //getting previous day's date
+                const date = new Date();
+                const today = `${date.getFullYear()}-0${date.getMonth() + 1}-${date.getDate() - 1}`;
+                //fetching cases data
+                console.log(`Getting data for ${data}`);
+                //Getting data from JHU
+                const response = await fetch(`https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/global?country=${data}&min_date=${today}&hide_fields=_id,%20country,%20country_code,%20country_iso2,%20country_iso3,%20loc,%20state,%20uid`)
+                if(response.status !== 200)
+                    console.log("Error occured when fetching data...");
+                else{
+                    var cases = await response.json();
+                    console.log(cases);
+                    console.log('latest confirmed:', cases[0].confirmed_daily);
+                }
+            }  
+        }catch(e){
+            console.log('Error from getData: ', e);
+        }
+    }
+
     return(
         <View style={globalStyle.screen}>
             {/* Header */}
@@ -11,11 +54,19 @@ export const Tracker = () =>{
             </View>
 
             {/* Body */}
-            <View>
+            <View style={{alignItems:'center'}}>
                 <TextInput
-                    placeholder="Username..."
+                    style={{marginTop: 30, fontSize: 20, marginBottom: 20, borderBottomWidth: 1}}
+                    placeholder="Country..."
+                    onChangeText={(text) => setcountry(text)}
                 />
+                {/* Storing country choices */}
+                <Button title='Store'  onPress={storeData}/>
+
+                {/* Getting latest data from JHU databse for chosen countries */}
+                <Button title='Get'  onPress={getData}/>
             </View>
+
         </View>
     )
 }
